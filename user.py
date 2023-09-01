edd305b1e11541d462f6d227d7f0b47cefebf2da
Ojobumiche <bumiche@mail.com>
2023-09-01 04:28:54 -0700

    user.py updated

--- user.py
+++ user.py
@@ -1,60 +1,55 @@
+#
 #!/usr/bin/python3
 """Defines the User class."""
-from base_model import BaseModel
 
+from base_model import BaseModel
 
 class User(BaseModel):
     """Represents a User.
 
     Attributes:
-        email (str): The email address of user.
+        email (str): The email address of the user.
         password (str): The password of the user.
         first_name (str): The first name of the user.
         last_name (str): The last name of the user.
     """
-    def __init__(self,username, email, password, first_name, last_name):
+
+    def __init__(self, username, email, password, first_name, last_name):
+        super().__init__()  # Call the constructor of the base class
         self.username = username
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
 
-    """user registration"""
-    def register():
+    @classmethod
+    def register(cls):
         username = input("Enter username: ")
         email = input("Enter your email: ")
         password = input("Enter your password: ")
         first_name = input("Enter your first name: ")
         last_name = input("Enter your last name: ")
-        return User(username, email, password, first_name, last_name)
+        return cls(username, email, password, first_name, last_name)
 
-        """User login"""
+    @staticmethod
     def login(users):
         email = input("Enter your email: ")
         password = input("Enter your password: ")
         username = input("Enter your username: ")
 
         for user in users:
-            if user.email == email and user.password == password:
+            if user.email == email and user.password == password and user.username == username:
                 print("Login successful!")
-            else:
-                 print("Invalid email or password.")
+                return
+        print("Invalid email, username, or password.")
 
-    """Register a new user"""
-    new_user = register()
+if __name__ == "__main__":
+    """ Register a new user"""
+    new_user = User.register()
 
-    """Store user objects in a list (simulating a database)
-    """
+    """ Store user objects in a list (simulating a database)"""
     users_list = [new_user]
 
-    """User login"""
-    login(users_list)
-
-
-
-
-
-user1 = User('email', 'password', 'username', 'first_name', 'last_name' )
-user1.register()
-print(user1.register)
+    """ User login"""
+    User.login(users_list)
 
