d325f2e68b080ad70befd9ac179ea06d606d1ecf
Ojobumiche <bumiche@mail.com>
2023-09-03 14:10:37 -0700

    ok

--- engine/storage.py
+++ engine/storage.py
@@ -7,22 +7,23 @@ from base_model import Users
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+"""Create Flask application instance and 
+store it in the app variable
+"""
 app = Flask(__name__)
 """mysql databaseURL"""
-app.config['SQLAlchemy_DATABASE_URL']= 'mysql:///swifterrandplus.db'
-db = SQLAlchemy['SQLAlchemy_TRACK_MODIFICATIONS'] = False
+"""MySQL database URL"""
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/swifterrandplus'
 
+db = SQLAlchemy['SQLAlchemy_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 """Create User Data Model.
 User model represent the user_data table"""
 
-class UserData(db.Model):
-    id = db.Column(db.integer,
-    primary_key= True)
-    email = db.Column(db.String(120),
-    unique=True, nullable=False)
-    tel = db.Column(db.String(15),
-    nullable=False) 
-    
-    
 
+class UserData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    tel = db.Column(db.String(15), nullable=False)
