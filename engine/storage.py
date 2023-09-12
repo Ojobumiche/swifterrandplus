80e15ac824cfa0cab75a169bd77ca6c94808d812
Ojobumiche <bumiche@mail.com>
2023-09-11 23:01:27 -0700

    ok

--- engine/storage.py
+++ engine/storage.py
@@ -3,7 +3,6 @@
 import os
 from sqlalchemy import Column, Integer, String, create_engine, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy.exc import IntegrityError
 from datetime import datetime
 
 # Create the SQLite database engine
@@ -21,8 +20,8 @@ class User(Base):
     user_id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False)
-    password = Column(String(25), nullable=False)  # Fixed typo here
-    phone_number = Column(Integer, nullable=False)  # Fixed typo here
+    password = Column(String(25), nullable=False)
+    phone_number = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
     def __str__(self):
@@ -32,9 +31,10 @@ class User(Base):
 # Create the table in the database
 Base.metadata.create_all(engine)
 
-"""Create a session to interact with the database"""
-session = sessionmaker(bind=engine)
-session = session()
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+
 # Define a function to create a new user login session
 
 
@@ -42,7 +42,7 @@ def create_user_session(username, password):
     try:
         # Check if the provided username and password match a user in the database
         user = session.query(User).filter_by(
-            username=username, password=password).first()
+            username=username, password=password).one()
 
         if user:
             # Session is created for the user
@@ -64,5 +64,5 @@ if __name__ == "__main__":
 
     # Call the function to create a user session
     user_session = create_user_session(username_input, password_input)
-    for user in user_session:
-        print(user)
+    if user_session:
+        print(user_session)
